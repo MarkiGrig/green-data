c569095971dd4fb2c572304e8c1e9e6824976a3e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-30 08:23:07 +0000

    Deploy MarkiGrig/green-data to github.com/MarkiGrig/green-data.git:gh-pages

---
+++ precache-manifest.3e19f6e0f7b8485d58ceabf414afa05c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83aec09876df763d524ad7d759eb26bd",
+    "url": "/green-data/index.html"
+  },
+  {
+    "revision": "b3b2c176c0305d52147d",
+    "url": "/green-data/static/css/main.2f3ba64f.chunk.css"
+  },
+  {
+    "revision": "d7b25ad2cf78165da59c",
+    "url": "/green-data/static/js/2.4b38296d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/green-data/static/js/2.4b38296d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b2c176c0305d52147d",
+    "url": "/green-data/static/js/main.ad16964a.chunk.js"
+  },
+  {
+    "revision": "e5e1b10607db17e93e11",
+    "url": "/green-data/static/js/runtime-main.f35cae05.js"
+  }
+]);
